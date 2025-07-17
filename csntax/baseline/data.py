1deacaa9fe4b28ed0850d2173def2ef2106ee664
Igor Sterner <igorsterner@gmail.com>
2025-07-17 23:09:36 +0100

    Update data.py

--- csntax/baseline/data.py
+++ csntax/baseline/data.py
@@ -4,22 +4,18 @@ import random
 import torch
 from torch.utils.data import Dataset
 
-
 class SentenceDataset(Dataset):
-    def __init__(self, filepath, tokenizer, max_length=128):
-        self.data = []
+    def __init__(self, data, tokenizer, max_length=128):
+        self.data = data
         self.tokenizer = tokenizer
         self.max_length = max_length
-        with open(filepath, "r") as f:
-            for line in f:
-                self.data.append(json.loads(line.strip()))
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         item = self.data[idx]
-        real_sentence = item["real_sentence"]
+        real_sentence = item["observed_sentence"]
         manipulated_sentence = item["manipulated_sentence"]
         if random.random() < 0.5:
             return real_sentence, manipulated_sentence, [1, 0]  # good first
@@ -31,6 +27,7 @@ class Collate:
     def __init__(self, tokenizer, device, max_length=128):
         self.tokenizer = tokenizer
         self.max_length = max_length
+        self.device = device
 
     def __call__(self, batch):
         sentences1, sentences2, labels = zip(*batch)
